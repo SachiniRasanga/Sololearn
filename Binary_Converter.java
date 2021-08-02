9e25dcd672c8e502bb1a5cbc074723c14d6d7ae9
Sachini Abeywardhana <87517026+SachiniRasanga@users.noreply.github.com>
2021-08-02 22:50:39 +0530

    Update Binary_Converter.java

--- Binary_Converter.java
+++ Binary_Converter.java
@@ -1,16 +1,6 @@
 import java.util.Scanner;
 
 //your code goes here
-
-
-public class Program {
-    public static void main(String[ ] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        System.out.print(Converter.toBinary(x));
-    }
-
-}
 public class Converter{
    public static String toBinary(int num){
 
@@ -22,5 +12,14 @@ public class Converter{
   return binary;
    }
 
+
+}
+
+public class Program {
+    public static void main(String[ ] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.print(Converter.toBinary(x));
+    }
 
 }
